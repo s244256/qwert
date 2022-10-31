79fef31477144f724b5b9cbdfe5eed47acbe38dd
student <chipsikleys3315@gmail.com>
2022-10-31 18:57:29 +0300

    first commit2

--- jfgjg.py
+++ jfgjg.py
@@ -1,2 +1,12 @@
-
-
+#Задание3, вариант 2.
+def maximum(mas):
+    return mas.index(max(mas))
+f = open("text.txt","r")
+n = f.readline()
+mas = []
+while n!="":
+    mas.append(float(n))
+    n = f.readline()
+print(str(min(mas)+max(mas)))
+f.close()
+print(maximum(mas))
